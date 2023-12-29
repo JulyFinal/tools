863d7f6bc82b57fa9ad6da0369b5b979e4bce8b5
julyfinal <final@julyfinals-MacBook-Pro.local>
2023-12-29 23:19:26 +0800

    nu

--- NSFW/av_tools.py
+++ NSFW/av_tools.py
@@ -302,6 +302,6 @@ def after_today_magnet():
 
 if __name__ == "__main__":
     engine = create_engine("sqlite:///final.db", echo=True)
-    # append_data_use_id(40639 + 1)
+    # append_data_use_id(40643 + 1)
     print("\n".join(after_today_magnet()))
     
