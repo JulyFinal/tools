78e65d6e1b758cd65611c08d4e55a0f8ca0d6fde
julyfinal <julyfinal@outlook.com>
2024-01-22 17:33:33 +0800

    nu

--- email_tools.py
+++ email_tools.py
@@ -2,19 +2,21 @@ import feedparser
 import pandas as pd
 import apprise
 
+user = ...
+passwd = ...
 
-def send_mail(link, title, content, tag):
+
+def send_mail(link, title, content):
     apobj = apprise.Apprise()
-    apobj.add("mailtos://")
-    apobj.notify(body=content, title=title, tag=tag)
+    apobj.add(f"mailtos://{user}:{passwd}@outlook.com", tag="email")
+    apobj.notify(body=content, title=title, tag="all")
 
 
 def rss_read(rss_url: str):
     news_feed = feedparser.parse(rss_url)
     df_news_feed = pd.json_normalize(news_feed.entries)
     for _, row in df_news_feed.iterrows():
-        print(row)
-        send_mail(row.link, row.title, row.summary, row.tags)
+        send_mail(row.link, row.title, row.tags + "\n" + row.summary)
 
     # print(df_news_feed)
 
